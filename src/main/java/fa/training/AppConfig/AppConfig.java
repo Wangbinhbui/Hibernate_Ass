d726b631d56d038d62325d4194e72997ab362b76
Wangbinhbui <quangbinhbui26038897@gmail.com>
2025-08-06 14:22:38 +0700

    appfig

--- src/main/java/fa/training/AppConfig/AppConfig.java
+++ src/main/java/fa/training/AppConfig/AppConfig.java
@@ -1,4 +1,57 @@
 package fa.training.AppConfig;
 
+import java.util.Properties;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages = "fa.training")
+@EnableTransactionManagement
 public class AppConfig {
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("fa.training.Entity");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        // 1. Sửa Driver Class cho SQL Server
+        dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        // 2. Sửa URL kết nối cho SQL Server
+        // Thêm encrypt=true và trustServerCertificate=true để tránh lỗi SSL với các driver mới
+        dataSource.setUrl("jdbc:sqlserver://localhost:1433;databaseName=HiHibernate_ass;encrypt=true;trustServerCertificate=true");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("123");
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        // 3. Sửa Dialect cho SQL Server để Hibernate tạo câu lệnh SQL chính xác
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLServer2012Dialect");
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.format_sql", "true");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update"); // Giữ nguyên: tự động cập nhật schema
+        return properties;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
 }
