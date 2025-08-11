5933db0de1ec19d62cb078dd951004e59a5ce815
Wangbinhbui <quangbinhbui26038897@gmail.com>
2025-08-11 21:48:56 +0700

    main

--- src/main/java/fa/training/Main.java
+++ src/main/java/fa/training/Main.java
@@ -1,13 +1,32 @@
 package fa.training;
 
-import org.hibernate.SessionFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import fa.training.dao.*;
+import fa.training.service.*;
+import java.time.LocalDate;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        SessionFactory sessionFactory = (SessionFactory) context.getBean("sessionFactory");
+        AuthorDao authorDao = new AuthorDaoImpl();
+        BookDao bookDao = new BookDaoImpl();
+        MemberDao memberDao = new MemberDaoImpl();
+        BorrowingDao borrowingDao = new BorrowingDaoImpl();
 
+        // Service
+        AuthorService authorService = new AuthorService(authorDao);
+        BookService bookService = new BookService(bookDao);
+        MemberService memberService = new MemberService(memberDao);
+        BorrowingService borrowingService = new BorrowingService(borrowingDao, bookDao, memberDao);
+
+        Long book1 = bookService.create("Clean Code");
+        Long book2 = bookService.create("Refactoring");
+        Long memberId = memberService.register("Alice", "alice@example.com", "0900000000");
+
+        borrowingService.borrowBooks(memberId, List.of(book1, book2), LocalDate.now().plusDays(7));
+        System.out.println("Done borrow!");
+
+        bookService.findAll().forEach(b ->
+                System.out.println(b.getId() + " - " + b.getTitle() + " - available=" + b.getAvailable())
+        );
     }
 }
